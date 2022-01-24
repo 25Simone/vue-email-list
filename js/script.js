90441f7736caaf02c6173aaa9b67a1aa9b9a2f6a
25Simone <dantsimone111@gmail.com>
2022-01-24 14:57:43 +0100

    add button to generate new list and css implementation

--- js/script.js
+++ js/script.js
@@ -3,12 +3,18 @@ new Vue({
     data: {
         emails: [],
     },
-    mounted(){
-        // Set a for loop to repeat the call 10 times
-        for(let i = 0; i < 10; i++){
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
+    methods: {
+        randomEmailsList: function() {
+            // Set a for loop to repeat the call 10 times
+            for(let i = 0; i < 10; i++){
+                this.emails = [];
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
                 this.emails.push(result.data.response);
             })
         }
+        },
+    },
+    mounted(){
+        this.randomEmailsList();
     },
 });
